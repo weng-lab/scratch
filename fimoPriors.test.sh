b224a579ce5c1e9d796f78fd663ad67149219f28
Michael Purcaro <purcaro@gmail.com>
2014-08-21 09:23:13 -0400

    add script

---
+++ fimoPriors.test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir -p purcaroFimoTest
+cd purcaroFimoTest
+
+# download a DNase-seq bigwig file from Encode
+wget -c https://www.encodeproject.org/files/ENCFF001BDH/@@download/ENCFF001BDH.bigWig
+
+# I'm just testing on chr7; convert bigWig to wig and extract chr7
+wget -c http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v287/bigWigToWig
+chmod u+x bigWigToWig
+./bigWigToWig -chrom=chr7 ENCFF001BDH.bigWig ENCFF001BDH.chr7.wig
+
+~/bin/fimo_4.10/bin/create-priors --alpha 1 --beta 10000 -oc Prior --num-bins 1000 ./ENCFF001BDH.chr7.wig
+
+# using CTCF from JASPAR 2014 from MEME site
+wget -c http://ebi.edu.au/ftp/software/MEME/Databases/motifs/motif_databases.12.1.tgz
+tar zxvf motif_databases.12.1.tgz
+
+wget -c http://hgdownload.cse.ucsc.edu/goldenpath/hg19/chromosomes/chr7.fa.gz
+gunzip chr7.fa.gz
+
+~/bin/fimo_4.10/bin/fimo --thresh 0.05 --text --parse-genomic-coord --psp ./Prior/priors.wig --prior-dist ./Prior/priors.dist --motif MA0139.1 ./motif_databases/JASPAR_CORE_2014_vertebrates.meme ./chr7.fa > fimo.priors.chr7.txt
+
+grep '+' ./fimo.priors.chr7.txt | grep -v '-' | wc -l
