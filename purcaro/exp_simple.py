f88f6d56d2bb9b4fa2ddc14a0446dce9ec7640da
Michael Purcaro <purcaro@gmail.com>
2016-11-30 13:55:57 -0500

    simple exp example

---
+++ purcaro/exp_simple.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import urllib
+import json
+
+class ExpFileSimple:
+    def __init__(self, fileJson):
+        self.fileJson = fileJson
+        self.file_type = self.fileJson["file_type"]
+        self.url = "https://www.encodeproject.org" + self.fileJson["href"]
+        self.technical_replicates = self.fileJson["technical_replicates"]
+        
+    def isBedNarrowPeak(self):
+        return "bed narrowPeak" == self.file_type
+
+class ExpSimple:
+    def __init__(self, accession):
+        self.accession = accession
+
+        url = "https://www.encodeproject.org/experiments/" + accession + "/?format=json"
+        response = urllib.urlopen(url)
+        self.expJson = json.loads(response.read())
+        
+    def getBedNarrowPeak(self):
+        for fileJson in self.expJson["files"]:
+            f = ExpFileSimple(fileJson)
+            if f.isBedNarrowPeak():
+                print(f.technical_replicates, f.url)
+        
+def main():
+    e = ExpSimple("ENCSR749BWV")
+    e.getBedNarrowPeak()
+
+if __name__ == "__main__":
+    sys.exit(main())
